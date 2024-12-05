f7e3efb202902bd3acf2615e4c525f838195489c
Sekhar <shivachirumamilla456@gmail.com>
2024-12-05 18:01:04 +0530

    add files

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cr7-tf-state"
-    kms_key_id = "ec2/tf.state"
+    key = "ec2/tf.state"
     region = "us-east-1"
     dynamodb_table = "cr-tf-state"
   }
