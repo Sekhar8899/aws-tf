65c3cd1f5ea0ecbb0c82a9a0ccb144d0442f4f33
Sekhar <shivachirumamilla456@gmail.com>
2024-12-05 20:18:15 +0530

    modified backend

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -4,8 +4,6 @@ terraform {
     key = "vpc/tf.state"
     dynamodb_table = "cr-tf-state"
     region = "us-east-1"
-    assume_role = {
-      role_arn = "arn:aws:iam::640168410393:role/GitHubActionsDeployerRole"
-    }
+
   }
 }
